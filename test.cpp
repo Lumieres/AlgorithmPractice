5e38ee203d85d7a54e809e2063ab79ff62380b95
Lumieres <liosho1535@gmail.com>
2021-09-19 10:23:24 +0900

    1-3 practice added

--- test.cpp
+++ test.cpp
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+void printStar(int x, int y, int size){
+    if(size ==1){
+        for(int i =0; i < size*3; i++){
+            
+        }
+    }
+}
+
 int main(void){
-    printf("hello world!!\n");
+
 }
