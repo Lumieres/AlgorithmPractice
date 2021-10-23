2cac554f4283eefcda316077ee185e2e3d2bb0c0
Lumieres <liosho1535@gmail.com>
2021-10-23 21:31:29 +0900

    chapter 3 add

--- practice/chapter3/3_1_2739_multiplecation.cpp
+++ practice/chapter3/3_1_2739_multiplecation.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void){
-	int N;
+	int Np
 	cin >> N;
 	for(int i=1; i<=9; i++){
 		cout << N << " * " << i << " = " << N*i << endl;
---
+++ practice/chapter3/3_2_10950_plus3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+	int T;
+	int a,b;
+	cin >> T;
+	for(int i=0; i < T; i++){
+		cin >> a >> b;
+		cout << a + b << endl;
+	}	
+}
